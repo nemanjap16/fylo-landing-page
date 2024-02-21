8b494f3a3aa7702297290d21c8372d50ef5000bf
nemanjap16 <necaintruder@gmail.com>
2024-02-21 19:13:19 +0100

    second form validation done

--- src/formValidation.ts
+++ src/formValidation.ts
@@ -1,27 +1,62 @@
+// hero form
+const heroForm = document.getElementById("hero-form") as HTMLFormElement;
 const heroButton = document.getElementById("hero-button") as HTMLButtonElement;
 const heroInput = document.getElementById("hero-input") as HTMLInputElement;
-const error = document.querySelector(".error-msg") as HTMLSpanElement;
-const heroForm = document.getElementById("hero-form") as HTMLFormElement;
+const error = document.getElementById("hero-error") as HTMLSpanElement;
+// access form
+const accessButton = document.getElementById(
+  "access-button"
+) as HTMLButtonElement;
+const accessInput = document.getElementById("access-input") as HTMLInputElement;
+const errorAccess = document.querySelector("#access-error") as HTMLSpanElement;
+const accessForm = document.getElementById("access-form") as HTMLFormElement;
 
 document.addEventListener("DOMContentLoaded", () => {
   error.hidden = true;
+  errorAccess.hidden = true;
 });
 
 const validEmail =
   /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
 
-heroButton.addEventListener("click", (e) => {
-  e.preventDefault();
-
-  if (heroInput.value !== "") {
+const showError = (id: string) => {
+  if (id == "hero-button") {
     if (heroInput.value.match(validEmail)) {
       error.hidden = true;
+      alert("Message sent!");
       heroForm.reset();
+    } else if (heroInput.value.length == 0) {
+      error.hidden = false;
+      alert("Invalid email!");
     }
-    return;
   }
 
-  if (heroInput.value == "") {
-    error.hidden = false;
+  if (id == "access-button") {
+    if (accessInput.value.match(validEmail)) {
+      errorAccess.hidden = true;
+      alert("Message sent!");
+      accessForm.reset();
+    } else if (accessInput.value.length == 0) {
+      errorAccess.hidden = false;
+      alert("Invalid email!");
+    }
   }
+};
+
+// hero form
+heroForm.addEventListener("click", (e: MouseEvent) => {
+  e.preventDefault();
+  e.stopPropagation();
+  const target = e.target as HTMLButtonElement;
+  showError(target.id);
+  console.log(target.id);
+});
+
+// access form
+accessForm.addEventListener("click", (e: MouseEvent) => {
+  e.preventDefault();
+  e.stopPropagation();
+  const target = e.target as HTMLButtonElement;
+  showError(target.id);
+  console.log(target.id);
 });
